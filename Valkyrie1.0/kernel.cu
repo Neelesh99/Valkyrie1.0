2fa98fed571f8c663188320a7311de3feb1e48e8
Neelesh99 <neelesh.ravichandran1999@gmail.com>
2021-02-04 14:44:34 +0000

    Added example GPU threading

--- Valkyrie1.0/kernel.cu
+++ Valkyrie1.0/kernel.cu
@@ -21,6 +21,49 @@ __global__ void addKernel(int *c, const int *a, const int *b)
     c[i] = a[i] + b[i];
 }
 
+__global__
+void add(int n, float* x, float* y) {
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = blockDim.x*gridDim.x;
+    for (int i = index; i < n; i+=stride) {
+        y[i] = x[i] + y[i];
+    }
+}
+
+int mainCudaTrial() {
+    int N = 1 << 20;
+
+    float* x, * y;
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
+
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    int blockSize = 256;
+
+    int numBlocks = (N + blockSize - 1) / blockSize;
+
+    // Run kernel on 1M elements on the CPU
+    add <<<numBlocks, blockSize >>> (N, x, y);
+
+    cudaDeviceSynchronize();
+
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i] - 3.0f));
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+
+    return 0;
+}
+
 int main()
 {
     //const int arraySize = 5;
@@ -66,13 +109,13 @@ int main()
     for (int i = 0; i < 4; i++) {
         std::cout << values2[i] << std::endl;
     }*/
-    for (int i = 0; i < 21; i++) {
+    /*for (int i = 0; i < 21; i++) {
         DeutschJozsa dj = DeutschJozsa();
         dj.buildResizableDeutschJozsa(100);
-    }    
+    } */   
     //delete(newQubit);
     //delete(space);
-    return 0;
+    return mainCudaTrial();
 }
 
 // Helper function for using CUDA to add vectors in parallel.
