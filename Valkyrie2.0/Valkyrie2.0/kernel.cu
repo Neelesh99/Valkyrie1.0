a539b374f338f1084e747555141b897fbe8e1a04
Neelesh99 <neelesh.ravichandran1999@gmail.com>
2021-05-05 16:26:26 +0100

    Added GPU identification information

--- Valkyrie2.0/Valkyrie2.0/kernel.cu
+++ Valkyrie2.0/Valkyrie2.0/kernel.cu
@@ -17,6 +17,7 @@
 using namespace antlr4;
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+void DisplayHeader();
 
 __global__ void addKernel(int *c, const int *a, const int *b)
 {
@@ -67,7 +68,7 @@ int main()
     Stager stage = Stager();
     std::vector<ConcurrentBlock> blocks = stage.stageInformation(registers, gateRequests);
 
-
+    DisplayHeader();
     return 0;
 }
 
@@ -149,4 +150,34 @@ Error:
     cudaFree(dev_b);
     
     return cudaStatus;
+}
+
+void DisplayHeader()
+{
+    const int kb = 1024;
+    const int mb = kb * kb;
+    std::cout << "NBody.GPU" << std::endl << "=========" << std::endl << std::endl;
+
+    std::cout << "CUDA version:   v" << CUDART_VERSION << std::endl;
+
+    int devCount;
+    cudaGetDeviceCount(&devCount);
+    std::cout << "CUDA Devices: " << std::endl << std::endl;
+
+    for (int i = 0; i < devCount; ++i)
+    {
+        cudaDeviceProp props;
+        cudaGetDeviceProperties(&props, i);
+        std::cout << i << ": " << props.name << ": " << props.major << "." << props.minor << std::endl;
+        std::cout << "  Global memory:   " << props.totalGlobalMem / mb << "mb" << std::endl;
+        std::cout << "  Shared memory:   " << props.sharedMemPerBlock / kb << "kb" << std::endl;
+        std::cout << "  Constant memory: " << props.totalConstMem / kb << "kb" << std::endl;
+        std::cout << "  Block registers: " << props.regsPerBlock << std::endl << std::endl;
+
+        std::cout << "  Warp size:         " << props.warpSize << std::endl;
+        std::cout << "  Threads per block: " << props.maxThreadsPerBlock << std::endl;
+        std::cout << "  Max block dimensions: [ " << props.maxThreadsDim[0] << ", " << props.maxThreadsDim[1] << ", " << props.maxThreadsDim[2] << " ]" << std::endl;
+        std::cout << "  Max grid dimensions:  [ " << props.maxGridSize[0] << ", " << props.maxGridSize[1] << ", " << props.maxGridSize[2] << " ]" << std::endl;
+        std::cout << std::endl;
+    }
 }
